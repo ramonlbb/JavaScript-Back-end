5da30984ed6565284212b662fce3dc70cff15330
ramonlbb <128474790+ramonlbb@users.noreply.github.com>
2024-08-28 20:50:05 -0300

    Update helloworld.js

--- Hello word/helloworld.js
+++ Hello word/helloworld.js
@@ -2,12 +2,12 @@ const expressFramework = require('express');
 const appInstance = expressFramework();
 const serverPort = 8080;
 
-// Define uma rota para a raiz do servidor
+
 appInstance.get('/', (request, response) => {
     response.send('Hello World');
 });
 
-// Inicia o servidor na porta especificada
+
 appInstance.listen(serverPort, () => {
     console.log(`http://localhost:${serverPort}`);
 });
